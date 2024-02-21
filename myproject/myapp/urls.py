ece69eb6930e3fc5e14d4af226797a0f9f93195a
Boris Dovbysh <boris.dovbysh@gmail.com>
2024-02-21 19:36:19 +0300

    task_6 done

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import main, about, get_user, get_all_users, user_orders, order_full, get_user_Moscow, get_user_Saint_Petersburg, product_form
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('get_user_Moscow/', get_user_Moscow, name='get_user_Moscow'),
     path('get_user_Saint_Petersburg/', get_user_Saint_Petersburg, name='get_user_Saint_Petersburg'),
     path('product_form/', product_form, name='product_form'),
+    #path('__debug__/', include("debug_toolbar.urls")),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,13 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-706wn*xtzrr_k*nxsao2pc8!(v^zh%(x8(+wtimw-7n0-p5@t5'
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    'MEDGORA.pythonanywhere.com',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -38,9 +48,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,8 +90,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MEDGORA$default',
+        'USER': 'MEDGORA',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'MEDGORA.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -119,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
